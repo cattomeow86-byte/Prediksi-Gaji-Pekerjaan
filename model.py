e7e09b1e311a231dab47588a6199aee99578cdef
cattomeow86-byte <cattomeow86@gmail.com>
2025-12-04 12:30:06 +0700

    model.py

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import joblib
 
 def load_model(filename='random_forest_regressor_model.pkl'):
-    """Memuat model yang sudah dilatih dari file random_forest_regressor_model.pkl."""
     try:
         model = joblib.load(filename)
         return model
